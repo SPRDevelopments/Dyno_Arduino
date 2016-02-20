1e5e34aad3fa17216ec6c9f0d7b857fdf158738d
SPowlesland <stevepowlesland2000@yahoo.com>
2016-02-20 18:26:50 +0000

    Test running smoothly

--- SerialTestDyno.ino
+++ SerialTestDyno.ino
@@ -3,59 +3,69 @@ String revs;
 String setupLine;
 boolean found ;
 int rev = 7200;
+int x;
+int increase;
+
 void setup() {
   pinMode(13, OUTPUT);
   // initialize serial:
   Serial.begin(115200);
   inputString = "";
-
+  increase = 1;
   //wait for "ff" and send a reply of "ff"
   while (found == false)
   {
     getFf();
   }
-  
-  inputString = "";
+  Serial.flush();
+  Serial.println(inputString);
+  delay(100);
+}
+
+void loop()
+{
+  GetRevs();
+//  Serial.println("Help");
   found = false;
   while (found == false)
   {
-    readLine();
-  }
-  Serial.print("Revs are ");
-  Serial.println(setupLine);
-  revs = setupLine;
-  rev = revs.toInt();
-  Serial.print("Rev is ");
-  Serial.println(rev);
+    GetStop();
+//    Serial.println("Help2");
+    if (increase == 1)
+    {
+      x = x + 111;
+      Serial.println(x);
+      delay(10);
+      if (x >= rev)
+      {
+        increase = 0;
+      }
+    }
+    if (increase == 0)
+    {
+      x = x - 100;
+      Serial.println(x);
+      delay(10);
+      if (x <= 51)
+      {
+        increase = 1;
+      }
+    }
+  }  
 }
-void loop()
-{
-  int x;
- 
-  for (x = 600; x < rev ; x = x + 200);
-  {
-    Serial.println( x);
-    delay(10);
-  }
-  for ( x = rev; x >= 600 ; x = x - 100)
-  {
-    Serial.println(x);
-    delay(20);
-  }
-}
-//Functions
+
+//Functions//
 void readLine()
 {
-  String x;
-  while (Serial.available())
+  if (Serial.available())
   {
-    x = Serial.readString();
-    setupLine = setupLine + x;
+    setupLine = Serial.readString();
+    if (setupLine.length() > 0)
+    {
+      found = true;
+    }
   }
-  if (setupLine.length() > 0)
-  {
-    found = true;
-  }
+
   else
   {
     found = false;
@@ -63,33 +73,51 @@ void readLine()
 }
 void getFf()
 {
-
-  char x;
-  while (Serial.available() > 0 )
+  if (Serial.available() > 0 )
   {
-    x = Serial.read();
-    inputString = inputString + x;
-  }
-
-  if (inputString != "ff")
-  {
-    Serial.println(inputString);
-    inputString = "";
-    found = false;
+    inputString = Serial.readString();
+    if (inputString = "ff")
+    {
+      
+      //inputString = "";
+      found = true;
+    }
   }
   else
   {
-
-    Serial.println(inputString);
-    //inputString = "";
-    found = true;
+    found = false;
   }
-
 }
 
-/*
+void GetRevs()
+{
+  inputString = "";
+  found = false;
+  while (found == false)
+  {
+    readLine();
+  }
+  revs = setupLine;
+  rev = revs.toInt();
+  Serial.flush();
+  Serial.println(rev);
+  // delay(3000);
+}
 
-*/
-
-
+void GetStop()
+{
+  if (Serial.available() > 0 )
+  {
+    inputString = Serial.readString();
+    if (inputString = "aa")
+    {
+      found = true;
+      Serial.flush();
+    }
+  }
+  else
+  {
+    found = false;
+  }
+}
 
